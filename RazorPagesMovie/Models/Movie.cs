2fd7670889f181d706674294af2c20fe58493b93
peter <peterwangyixing@gmail.com>
2025-02-12 15:09:14 +0800

    Add new data annotations

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -6,12 +6,22 @@ namespace RazorPagesMovie.Models;
 public class Movie
 {
     public int Id { get; set; }
-    public string? Title { get; set; }
+    [Required]
+    [StringLength(60, MinimumLength = 3)]
+    public string? Title { get; set; } = string.Empty;
     [DataType(DataType.Date)]
     [Display(Name = "Release Date")]
     public DateTime ReleaseDate { get; set; }
-    public string? Genre { get; set; }
+    [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+    [Required]
+    [StringLength(30)]
+    public string? Genre { get; set; } = string.Empty;
     [Column(TypeName = "decimal(18, 2)")]
+    [DataType(DataType.Currency)]
+    [Range(1, 100)]
     public decimal Price { get; set; }
+    [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+    [StringLength(5)]
+    [Required]
     public string Rating { get; set; } = string.Empty;
 }
